1543adce9b389f71b3b8eb92d0e10c81ec47fe06
tianzhi-george-li <tianzhi-george-li@users.noreply.github.com>
2022-01-04 18:22:32 -0500

    Day15 part1

--- Day15/chiton_1.py
+++ Day15/chiton_1.py
@@ -1,39 +1,57 @@
 def main():
-    with open("Day15\inputTest.txt") as f:
+    with open("Day15\input.txt") as f:
         input = f.readlines()
     input = parse_input(input)
-    size = len(input)
-    start, end = (0, 0), (size-1, size-1)
-    accepted_values = [(x, y) for x in range(size) for y in range(size)]
-    shortest_path(input, start, end, [start], accepted_values)
-    print(memo[(start, end)])
 
+    grid_size = len(input)
+    start, end = (0, 0), (grid_size - 1, grid_size - 1)
+    vertices = [(x, y) for x in range(grid_size) for y in range(grid_size)]
+    edges = get_edges(input, vertices)
+
+    result = bellman_ford(vertices, edges, start)
+
+    print(result[0][end])
+    
 def parse_input(input):
     input = list(map(lambda x: x.strip(), (row for row in input)))
     input = list(map(lambda x: list(x), (row for row in input)))
     input = [list(map(int, (digit for digit in row))) for row in input]
     return input
 
-def shortest_path(grid, start, end, seen, accepted):
-    if start == end:
-        return 0
-    elif (start, end) in memo:
-        return memo[(start, end)]
-    else:
-        end_x, end_y = end[0], end[1]
-        end_neighbours = [(end_x, end_y+1), (end_x, end_y-1), (end_x+1, end_y), (end_x-1, end_y)]
-        paths = []
-        print(end_neighbours)
-        for node in end_neighbours:
-            if node in accepted and node not in seen:
-                new_seen = seen.copy()
-                new_seen.append(node)
-                test = shortest_path(grid, start, node, new_seen, accepted)
-                print(f'{test = }')
-                path = shortest_path(grid, start, node, new_seen, accepted) + grid[node[0]][node[1]]
-                paths.append(path)
-        memo[(start, node)] = min(paths, default = 0)
+def get_edges(grid, V):
+    edges = {}
+    for v in V:
+        neighbours = [(v[0], v[1]+1), (v[0], v[1]-1), (v[0]+1, v[1]), (v[0]-1, v[1])]
+        for node in neighbours:
+            if node in V:
+                weight = get_risk(grid, node)
+                edges[(v, node)] = weight
+    return edges
+
+def get_risk(grid, node):
+    row = node[0]
+    col = node[1]
+    return grid[row][col]
+
+def bellman_ford(vertices, edges, source):
+    distance = {}
+    predecessor = {}
+
+    for v in vertices:
+        distance[v] = 9999
+        predecessor[v] = None
+
+    distance[source] = 0
+
+    for i in range(len(vertices)-1):
+        for e in edges:
+            u, v = e[0], e[1]
+            w = edges[e]
+            if distance[u] + w < distance[v]:
+                distance[v] = distance[u] + w
+                predecessor[v] = u
+
+    return distance, predecessor
 
 if __name__ == "__main__":
-    memo = {}
     main()
